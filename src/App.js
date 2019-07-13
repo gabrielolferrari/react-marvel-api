a71031ae12730331dbe2fb3cf2f5eb62ffb5718d
gabrielolferrari <gabrielolferrari@gmail.com>
2019-07-12 22:49:03 -0300

    Add Material UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import CharactersList from './components/characters';
 
 const App = () => (
-  <CharactersList />
+  <React.Fragment>
+    <AppBar position="relative">
+      <Toolbar>
+        <Typography variant="h6" color="inherit" noWrap>
+          MARVEL
+        </Typography>
+      </Toolbar>
+    </AppBar>
+    <main>
+      <CharactersList />
+    </main>
+  </React.Fragment>
 )
 
 export default App;
