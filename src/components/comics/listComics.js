4f9c02017a17a6d20283fb28dfdbe95a00c3d0f2
gabrielolferrari <gabrielolferrari@gmail.com>
2019-07-13 09:55:08 -0300

    Convert listComics to a functional component

--- src/components/comics/listComics.js
+++ src/components/comics/listComics.js
@@ -1,31 +1,54 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
 import { getComics } from '../../services/MarvelAPI';
 import CardComic from './cardComic';
 
-export default class ListComics extends Component {
-  state = {
-    comics: [],
-  };
+const ListComics = props => {
 
-  async componentDidMount() {
+  const useStyles = makeStyles(theme => ({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'space-around',
+      overflow: 'hidden',
+      backgroundColor: theme.palette.background.paper,
+    },
+    gridList: {
+      width: 500,
+      height: 450,
+    },
+    icon: {
+      color: 'rgba(255, 255, 255, 0.54)',
+    },
+  }));
+
+  const [comics, setComics] = useState([]);
+
+  const classes = useStyles();
+
+  async function fetchMyAPI() {
     try {
-      await getComics().then((response) => {
-        this.setState({ comics: response.data.data.results });
-       });
+      const result = await getComics();
+      setComics(result.data.data.results);
     } catch (e) {
       console.log(`Request failed: ${e}`);
     }
   }
 
-  render() {
-    return (
-      <React.Fragment>
-      {
-        this.state.comics.map((comic) =>
-        <CardComic key={comic.id.toString()} info={comic} />
-        )
-      }
-      </React.Fragment>
-    );
-  }
+  useEffect(() => {
+    fetchMyAPI();
+  }, []);
+
+  return (
+    <React.Fragment className={useStyles}>
+    {
+      comics.map((comic) =>
+      <CardComic key={comic.id.toString()} info={comic} />
+      )
+    }
+    </React.Fragment>
+  );
 }
+
+export default ListComics
